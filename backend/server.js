013628c9af44232ff7e05fef5725a24e9204859b
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-03-08 12:53:14 -0600

    conection to mongo

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,15 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+const connectDB = require('./config/db')
+
+//Load env vars
+
+dotenv.config()
+
+//Connect to database
+
+connectDB()
 
 const app = express()
 
@@ -8,5 +19,18 @@ app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
-const PORT = 5000
-app.listen(PORT, console.log(`Server running in 5000 mode on port 5000`))
+const PORT = process.env.PORT || 5000
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
+  )
+)
+
+//Handle inhandled promises rejections
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  //close server and exit process
+  server.close(() => process.exit(1))
+})
